a25b60bf88847e1c0ddbb1142256ec4664556145
Sebastian <ksiezak.sebastian@gmail.com>
2024-04-11 09:59:46 +0200

    add: arrow_drop_down.svg and search.svg icons | create: SearchBar.tsx component with css styling | update: AppHeader.tsx with SearchBar component, adjust styling |

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,6 +3,7 @@ import styles from "./AppHeader.module.css";
 
 import amzLogo from "../assets/logo-white.svg";
 import DeliverTo from "./DeliverTo";
+import SearchBar from "./SearchBar";
 
 function AppHeader() {
   return (
@@ -10,14 +11,12 @@ function AppHeader() {
       <div id={styles.navbar} role="navigation" aria-label="navigation">
         <div id={styles["nav-belt"]}>
           <div className={styles.navLeft}>
-            <div>
-              <img src={amzLogo} className={styles.logo} alt="Amazon logo" />
-            </div>
-            <div>
-              <DeliverTo />
-            </div>
+            <img src={amzLogo} className={styles.logo} alt="Amazon logo" />
+            <DeliverTo />
           </div>
-          <div className={styles.navFill}>Fill</div>
+          <div className={styles.navFill}>
+            <SearchBar />
+          </div>
           <div className={styles.navRight}>Right</div>
         </div>
       </div>
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,23 @@
+import "./SearchBar.module.css";
+import styles from "./SearchBar.module.css";
+
+import searchIcon from "../assets/search.svg";
+import arrowDropDown from "../assets/arrow_drop_down.svg";
+
+function SearchBar() {
+  return (
+    <div className={styles.container}>
+      <label htmlFor="search">Search Amazon React SPA</label>
+      <button className={styles.categoryBtn}>
+        <span>All</span>
+        <img src={arrowDropDown} alt="Arrow drop down icon" />
+      </button>
+      <input type="text" name="search" placeholder="Search Amazon React SPA" />
+      <button className={styles.searchBtn}>
+        <img src={searchIcon} alt="Search icon" />
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
